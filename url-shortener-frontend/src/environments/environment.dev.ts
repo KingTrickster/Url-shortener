4ba1cc3cb6907cc68b8ef69b2d2c50ae207288b9
Alejandro Trejo <alextrejores@gmail.com>
2023-05-09 18:47:39 -0600

    Updated env files

---
+++ url-shortener-frontend/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  title: 'Dev Environment Heading',
+  apiURL: 'http://localhost:8079/api/v1/url-shortener'
+};
---
+++ url-shortener-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+
+  production: true,
+
+  title: 'Production Environment Heading',
+
+  apiURL: 'https://url-shortener-server-o86f.onrender.com/api/v1/url-shortener'
+
+};
