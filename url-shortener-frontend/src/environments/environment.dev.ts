630b22e689d2a64b393c5647f1fccd21d813bb57
Alejandro Trejo <alextrejores@gmail.com>
2023-05-09 18:25:50 -0600

    Added env to git ignore

--- url-shortener-frontend/src/environments/environment.dev.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  title: 'Dev Environment Heading',
-  apiURL: 'https://url-shortener-server-o86f.onrender.com/api/v1/url-shortener'
-};
--- url-shortener-frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-
-  production: true,
-
-  title: 'Production Environment Heading',
-
-  apiURL: 'https://url-shortener-server-o86f.onrender.com/api/v1/url-shortener'
-
-};
